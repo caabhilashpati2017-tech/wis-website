e8464968085d6b04243a0a85c55889ecc1b19ba1
caabhilashpati2017-tech <caabhilashpati2017@gmail.com>
2026-05-02 16:45:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 export default function App() {
   return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>WIS Website Live</h1>
+    <div style={{ padding: "50px", fontFamily: "Arial" }}>
+      <h1>WIS Website Live ✅</h1>
       <p>Wealthverse Investment Solution</p>
+      <p>By Abhilash Pati (CA)</p>
     </div>
-  )
+  );
 }
